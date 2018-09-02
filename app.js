6afb7cc8fd2554140b4bf87f8600d40a6dc44c6b
Utkarsh <utkarshengg1984@gmail.com>
2018-09-02 19:56:06 +0530

    second

--- app.js
+++ app.js
@@ -13,23 +13,23 @@ app.use(cors());
 // console.log(process.env.CONNECTION_STRING);
 // connect to mlab database
 // make sure to replace my db string & creds with your own
-mongoose.connect(
-  `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${
-    process.env.DB_HOST
-  }`
-);
-mongoose.connection.once("open", () => {
-  console.log("conneted to database");
-});
+// mongoose.connect(
+//   `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${
+//     process.env.DB_HOST
+//   }`
+// );
+// mongoose.connection.once("open", () => {
+//   console.log("conneted to database");
+// });
 
-// bind express with graphql
-app.use(
-  "/graphql",
-  graphqlHTTP({
-    schema,
-    graphiql: true
-  })
-);
+// // bind express with graphql
+// app.use(
+//   "/graphql",
+//   graphqlHTTP({
+//     schema,
+//     graphiql: true
+//   })
+// );
 
 app.listen(4000, () => {
   console.log("now listening for requests on port 4000");
